35277138fc1bed732366d5ccc031ff3f27881e41
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-31 21:43:24 +0200

    Third exercise: rename variable

--- ruby_basics_2/3_fibonacci_array_filling.rb
+++ ruby_basics_2/3_fibonacci_array_filling.rb
@@ -1,12 +1,12 @@
 fibonacci_array = [0, 1]
-new_value = 0
-while new_value <= 100
+next_value = 0
+while next_value <= 100
   first_operand = fibonacci_array[fibonacci_array.length - 1]
   second_operand = fibonacci_array[fibonacci_array.length - 2]
-  new_value = first_operand + second_operand
+  next_value = first_operand + second_operand
 
-  if new_value <= 100
-    fibonacci_array.push(new_value)
+  if next_value <= 100
+    fibonacci_array.push(next_value)
   end
 end
 
