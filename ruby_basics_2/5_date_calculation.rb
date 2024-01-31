9655a81e016cd3222c15622b77306eefd14ffbbf
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-01-31 21:40:38 +0200

    Fifth exercise

--- ruby_basics_2/5_date_calculation.rb
+++ ruby_basics_2/5_date_calculation.rb
@@ -1,23 +1,29 @@
+months = {
+  january: 31,
+  february: 28,
+  march: 31,
+  april: 30,
+  may: 31,
+  june: 30,
+  july: 31,
+  august: 31,
+  september: 30,
+  october: 31,
+  november: 30,
+  december: 31
+}
+
 puts 'Enter a date'
-day = gets.chomp.to_i
+days = gets.chomp.to_i
 puts 'Enter a number of month (from 1 to 12)'
 month = gets.chomp.to_i
 puts 'Enter a year'
 year = gets.chomp.to_i
 
-year_leap = false
+is_year_leap = (year % 4 === 0) && (!(year % 100 === 0) || (year % 400 === 0))
+previous_month_list = month == 1 ? [] : months.values[0..(month - 2)]
+months[:february] = is_year_leap ? '29' : '28';
 
-if year % 4 == 0
-  if year % 100 == 0
-    if year % 400 == 0
-      year_leap = true
-    else
-      year_leap = false
-    end
-  else
-    year_leap = true
-  end
-end
+total_days = previous_month_list.sum + days
 
-puts year
-puts year_leap
+puts total_days
