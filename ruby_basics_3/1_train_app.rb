5b6d42970bb0baab9afe48cafad5113e0ef31a56
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2024-02-11 11:14:39 +0200

    feat: add station class

--- ruby_basics_3/1_train_app.rb
+++ ruby_basics_3/1_train_app.rb
@@ -1,22 +1,27 @@
-# Класс Station (Станция):
-#   Имеет название, которое указывается при ее создании
-# Может принимать поезда (по одному за раз)
-# Может возвращать список всех поездов на станции, находящиеся в текущий момент
-# Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
-# Может отправлять поезда (по одному за раз, при этом, поезд удаляется из списка поездов, находящихся на станции).
 class Station
   def initialize(station_name)
     @station_name = station_name
     @station_trains = []
   end
 
-  def station_trains(train_type)
-    @station_trains[train_type]
-  end
-
-  def add_station_train(train)
+  def station_train=(train)
     @station_trains << train
   end
+
+  def station_trains
+    @station_trains
+  end
+
+  def station_trains_by_type(type)
+    @station_trains.select do |train|
+      train.type === type
+    end
+  end
+
+  def dispatch_train
+    @station_trains.pop
+  end
+
 end
 #   Класс Route (Маршрут):
 #   Имеет начальную и конечную станцию, а также список промежуточных станций. Начальная и конечная станции указываютсся при создании маршрута, а промежуточные могут добавляться между ними.
